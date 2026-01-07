f2bb68397cfc9a45c900399fa5f80d202a4af087
termul488-ai <termul488@gmail.com>
2026-01-08 00:31:56 +0700

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -61,7 +61,7 @@ def handleStatusCodes(status_code):
 	global request_counter
 	sys.stdout.write("\033[100m \033[40m\r%i Target\033[0m" % request_counter) 
 	sys.stdout.flush()
-	print(f"\033[33m" +str(url)+ "\033[38;5;1m runing..!!\033[0m")
+	print(f"\033[33m  "+str(url)+"\033[38;5;1m runing\033[0m")
 	if status_code == 429:
 			printMsg("You have been throttled")
 	if status_code == 500:
